4c56332c949e4341f577378450e2a9f1efd462c4
Abdul Rahaman <abduldevelop@gmail.com>
2024-09-22 10:01:01 -0700

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ configDotenv();
 
 // Initialize Express app and Prisma client
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(
   cors({
